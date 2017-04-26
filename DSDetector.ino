326d72cc1b42a30f9ece401b6f84d4d9159db51b
Maycon Rafael Campos Borba <mayconrafaelcb_@hotmail.com>
2017-04-26 01:49:41 -0300

    Implementação Sensor de Temperatura
    dia 26/04/2017

--- DSDetector.ino
+++ DSDetector.ino
@@ -7,26 +7,28 @@
 #include "SensorTemperatura.hpp"
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <Wire.h>
-#include <Arduino.h>
 
 //------------------------------PINOS-----------------------------------------------------
- int PinoTemperatura = 13;//Pino Temperatura
+ int pinoTemperatura = 13;//Pino Temperatura
  
-//----------------------------------------------------------------------------------------
-  Includes *v1;
- 
-//------------------------------Iniciando Sensor Temperatura
- /* OneWire sensorTemp(getPinoSensorTemperatura());
-  DallasTemperature sensors(sensorTemp);
-  DeviceAddress sensor1;*/
-//---------------------------------------------------------------------------------------
+//------------------------------PONTEIROS-------------------------------------------------
+  SensorTemperatura *sensorTemp;
+  OneWire oneWire(pinoTemperatura);
+  DallasTemperature sensors(&oneWire);
+  DeviceAddress sensor1;
+//------------------------------SETUP-----------------------------------------------------
 void setup(){
-  v1 = new Includes();
-  v1->setPinoSensorTemperatura(13);
-  //Serial.begin(9600);
- // sensors.begin();
-}
-void loop(){
+  sensorTemp = new SensorTemperatura(pinoTemperatura);
+  Serial.begin(9600);
+  sensors.begin();
 
+}
+//------------------------------LOOP-----------------------------------------------------
+void loop(){
+  if(sensorTemp->verificarSensorTemp(sensors,sensor1)){
+    sensorTemp->celsius(sensors,sensor1);
+    sensorTemp->imprimirTemp();
+    
+  }
+  delay(500);
 }
--- SensorTemperatura.hpp
+++ SensorTemperatura.hpp
@@ -1,27 +1,99 @@
 #ifndef SENSORTEMPERATURA_HPP
 #define SENSORTEMPERATURA_HPP
 //--------------------------------------------------------------------------
-#include "Includes.hpp"
+#include <OneWire.h>
 #include <DallasTemperature.h>
-
 //--------------------------------------------------------------------------
 class SensorTemperatura{
    public:
-      SensorTemperatura();
-      void VerificaSensor();
+      SensorTemperatura(int pino);
+      bool verificarSensorTemp(DallasTemperature sensors,DeviceAddress sensor1);
+      void mostrarEndereco(DeviceAddress sensor1);
+      float getTempMin();
+      float getTempMax();
+      float getTempAtual();
+      void setTempMin(float tempMin);
+      void setTempMax(float tempMax);
+      void celsius(DallasTemperature sensors,DeviceAddress sensor1);
+      void imprimirTemp(); 
   private:
-    float tempMin = 999;
-    float tempMax = 0;
-    float tempAtual = 0;
+    float tempMin;
+    float tempMax;
+    float tempAtual;
+    int pino;
 };
 
-SensorTemperatura::SensorTemperatura(){
+SensorTemperatura::SensorTemperatura(int pino){
+      this->pino = pino;
+      this->tempMin=999;
+      this->tempMax=0;
 }
 
-
-void SensorTemperatura::VerificaSensor(){
- 
+float SensorTemperatura::getTempMin(){
+  return this->tempMin;
 }
 
+float SensorTemperatura::getTempAtual(){
+  return this->tempAtual;
+}
+
+float SensorTemperatura::getTempMax(){
+  return this->tempMax;
+}
+
+void  SensorTemperatura::setTempMax(float tempMax){
+  this->tempMax = tempMax;
+}
+
+void  SensorTemperatura::setTempMin(float tempMin){
+  this->tempMin = tempMin;
+}
+
+ bool SensorTemperatura::verificarSensorTemp(DallasTemperature sensors, DeviceAddress sensor1){
+  if(!sensors.getAddress(sensor1,0)){
+    Serial.println("Sensor de Temperatura nao encontrado!");
+    Serial.println();
+    return false;
+  }else{
+    Serial.println("Sensor de Temperatura encontrado!");
+    Serial.print("Endereco: ");
+    mostrarEndereco(sensor1);
+     Serial.println();
+    return true;
+  }
+  
+ }
+
+ void SensorTemperatura::mostrarEndereco(DeviceAddress sensor1){
+    for(uint8_t i=1; i<8;i++){
+      if(sensor1[i]<16){
+        Serial.print("0");
+      }else{
+        Serial.print(sensor1[i],HEX);
+      }
+    }
+ }
+
+void SensorTemperatura::celsius(DallasTemperature sensors,DeviceAddress sensor1){
+    sensors.requestTemperatures();
+    float tempCelsius = sensors.getTempC(sensor1);
+    if(tempCelsius<this->tempMin){
+      this->tempMin = tempCelsius;
+    }
+    if(tempCelsius>this->tempMax){
+        this->tempMax = tempCelsius;
+    }
+    this->tempAtual = tempCelsius;
+ }
+
+void SensorTemperatura::imprimirTemp(){
+  Serial.print("Temperatura: ");
+  Serial.print(getTempAtual());
+  Serial.print("ºC Min: ");
+  Serial.print(getTempMin());
+  Serial.print("ºC Max: ");
+  Serial.print(getTempMax());
+   Serial.println("ºC");
+}
 #endif
 
